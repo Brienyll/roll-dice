6c35d22d8a467eb54f50fa24fbdb9282ea74703d
Brienyll <Brienyll29@gmail.com>
2019-05-27 20:02:02 -0700

    initial commit

---
+++ src/RollDice.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class RollDice extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return <i class="fas fa-dice-one"></i>
+  }
+}
+
+export default RollDice;
